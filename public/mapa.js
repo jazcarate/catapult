3eb7b2b20dd45e9da1a315dac35e7be570e691e5
Joaquin Azcarate <theone.games@gmail.com>
2016-12-06 02:02:12 +0000

    MVP horrible

--- public/mapa.js
+++ public/mapa.js
@@ -5,16 +5,21 @@ function Mapa(mapaID, opciones){
     this.mapa = new google.maps.Map(
         document.getElementById(mapaID),
         {
+            center: {
+                lat: -34.60368440000001,
+                lng: -58.381559100000004
+            },
             'zoom':12,
             'mapTypeId': google.maps.MapTypeId.ROADMAP
         }
     );
+    
     this.ser = new google.maps.DirectionsService();
 }
 
 Mapa.prototype = {
     obtenerRenderer: function(opciones){
-        var ren = new google.maps.DirectionsRenderer(
+        this.ren = new google.maps.DirectionsRenderer(
             Object.assign(
                 {
                     'draggable':  false
@@ -22,8 +27,11 @@ Mapa.prototype = {
                 opciones
             )
         );
-        ren.setMap(this.mapa);
-        return ren;
+        this.ren.setMap(this.mapa);
+        return this.ren;
+    },
+    getUltimoRen: function(){
+        return this.ren;
     },
     
     ruta: function(origen, destino){
@@ -41,9 +49,67 @@ Mapa.prototype = {
         );
     },
     
+    autocomplete: function(inputId){
+        var self = this;
+        var input = document.getElementById(inputId);
+        var searchBox = new google.maps.places.SearchBox(input);
+        self.mapa.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        self.mapa.addListener('bounds_changed', function() {
+          searchBox.setBounds(self.mapa.getBounds());
+        });
+        
+        var markers = [];
+        searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+
+          //Borrar los marcadores
+          markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = [];
+
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("El lugar no tiene geometria.");
+              return;
+            }
+
+            var poi = new google.maps.Marker({
+              draggable: true,
+              map: self.mapa,
+              title: place.name,
+              position: place.geometry.location
+            });
+            
+            markers.push(poi);
+            self.setMarca(place.geometry.location);
+            
+            google.maps.event.addListener(poi, 'dragend', function(e) {
+                self.setMarca(this.getPosition());
+            });
+
+            if (place.geometry.viewport) {
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          self.mapa.fitBounds(bounds);
+        });
+    },
+    
+    setMarca: function(pos){
+        this.marca = pos.toString();
+    },
+    
     guardar: function(){
         var w=[], data={};
-        var rleg = this.ren.directions.routes[0].legs[0];
+        var rleg = this.getUltimoRen().directions.routes[0].legs[0];
         data.start = {'lat': rleg.start_location.lat(), 'lng':rleg.start_location.lng()};
         data.end = {'lat': rleg.end_location.lat(), 'lng':rleg.end_location.lng()};
         var wp = rleg.via_waypoints;
