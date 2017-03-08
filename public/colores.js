39b5de68385607f0ac64a95bbcc72e437886e0cc
Joaquin Azcarate <theone.games@gmail.com>
2017-03-08 16:54:57 +0000

    agregar colores

--- public/colores.js
+++ public/colores.js
@@ -1,4 +1,4 @@
-var colores = ['#FE7569', '#9e19f3', '#00d719', '#5af0e6', '#c16db9' ];
+var colores = ['#FE7569', '#9e19f3', '#00d719', '#5af0e6', '#c16db9', '#3cd2d0', '#fa2aac', '#a082ae', '#3cd2d0', '#7585af', '#984775', '#35f469', '#35f469', '#1963a2', '#f5e200' ];
 
 function obtenerColor(i){
     return colores[i % colores.length];
